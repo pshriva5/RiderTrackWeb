617b0075abeb6ea7b25da2dc09aea6d337837ef6
Himani Shah <hjshah2@asu.edu>
2018-01-21 19:02:28 -0700

    Almost there the UI and routes working
    Some problems with mongoDB atlas connection

--- Authentication/models/user.js
+++ Authentication/models/user.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User',{
+    id: String,
     username: String,
     password: String,
-    email: String
+    email: String,
+    firstName: String,
+    lastName: String
 });
